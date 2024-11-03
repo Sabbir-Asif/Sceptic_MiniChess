a8abe8aabe4a8b4dee2fac7fc39f782324d3b253
Sabbir-Asif <bsse1333@iit.du.ac.bd>
2024-11-03 00:03:42 -0700

    drag and drop added

--- frontend/src/components/chessBoard/MiniChessBoard.jsx
+++ frontend/src/components/chessBoard/MiniChessBoard.jsx
@@ -11,29 +11,30 @@ import whiteBishop from '/images/wB.png';
 import whiteKnight from '/images/wN.png';
 import whiteRook from '/images/wR.png';
 import whitePawn from '/images/wP.png';
+import useDragAndDrop from './helper/DragAndDrop';
 
 const initialBoard = [
-  ['blackKing', 'blackQueen', 'blackBishop', 'blackKnight', 'blackRook'],
-  ['blackPawn', 'blackPawn', 'blackPawn', 'blackPawn', 'blackPawn'],
+  ['bk', 'bq', 'bb', 'bn', 'br'],
+  ['bp', 'bp', 'bp', 'bp', 'bp'],
   ['.', '.', '.', '.', '.'],
   ['.', '.', '.', '.', '.'],
-  ['whitePawn', 'whitePawn', 'whitePawn', 'whitePawn', 'whitePawn'],
-  ['whiteRook', 'whiteKnight', 'whiteBishop', 'whiteQueen', 'whiteKing']
+  ['wp', 'wp', 'wp', 'wp', 'wp'],
+  ['wr', 'wn', 'wb', 'wq', 'wk']
 ];
 
 const pieceImages = {
-  blackKing,
-  blackQueen,
-  blackBishop,
-  blackKnight,
-  blackRook,
-  blackPawn,
-  whiteKing,
-  whiteQueen,
-  whiteBishop,
-  whiteKnight,
-  whiteRook,
-  whitePawn,
+  bk: blackKing,
+  bq: blackQueen,
+  bb: blackBishop,
+  bn: blackKnight,
+  br: blackRook,
+  bp: blackPawn,
+  wk: whiteKing,
+  wq: whiteQueen,
+  wb: whiteBishop,
+  wn: whiteKnight,
+  wr: whiteRook,
+  wp: whitePawn,
 };
 
 const files = ['a', 'b', 'c', 'd', 'e'];
@@ -41,11 +42,11 @@ const ranks = ['6', '5', '4', '3', '2', '1'];
 
 const MiniChessBoard = () => {
   const [board, setBoard] = useState(initialBoard);
-  const [history, setHistory] = useState([]);
+  const { onDragStart, onDrop, onDragEndOutsideBoard } = useDragAndDrop(board, setBoard);
 
   return (
     <div className="max-w-5xl mx-auto flex flex-col items-center">
-      <h1 className="text-4xl font-bold mb-8 text-green-secondary">5x6 Chess Game</h1>
+      <h1 className="text-4xl font-bold mb-6 text-green-secondary">5x6 Chess Game</h1>
       <div className="relative">
         <div className="absolute -left-5 top-0 flex flex-col">
           {ranks.map((rank) => (
@@ -61,14 +62,24 @@ const MiniChessBoard = () => {
                 key={`${rowIndex}-${colIndex}`}
                 className={`w-24 h-24 flex items-center justify-center border 
                 ${(rowIndex + colIndex) % 2 === 0 ? 'bg-green-primary' : 'bg-green-info'}`}
+                onDragOver={(e) => e.preventDefault()}
+                onDrop={() => onDrop(rowIndex, colIndex)}
               >
                 {cell !== '.' && (
-                  <img src={pieceImages[cell]} alt={cell} className="w-20 h-20" />
+                  <img
+                    src={pieceImages[cell]}
+                    alt={cell}
+                    className="w-20 h-20"
+                    draggable
+                    onDragStart={() => onDragStart(cell, rowIndex, colIndex)}
+                    onDragEnd={onDragEndOutsideBoard}
+                  />
                 )}
               </div>
             ))
           )}
         </div>
+        
         <div className="absolute -bottom-7 left-10 flex">
           {files.map((file) => (
             <div key={file} className="w-24 text-start text-base-200 font-bold">
